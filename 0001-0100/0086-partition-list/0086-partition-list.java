4e8e49c4068af188d661b30500e8fa5d589d94b6
Chaitanya Kher <chaitanyakher2131996@gmail.com>
2026-09-22 07:33:58 +0530

    Solved 0086: Partition List

---
+++ 0001-0100/0086-partition-list/0086-partition-list.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        if (head==null){
+            return null;
+        }
+
+        ListNode dummy1 = new ListNode(0);
+        ListNode smaller = dummy1;
+        ListNode dummy2 = new ListNode(0);
+        ListNode bigger = dummy2;
+
+        while (head!= null) {
+            if (head.val<x) {
+                smaller.next = head;
+                smaller = smaller.next;
+            } else {
+                bigger.next = head;
+                bigger = bigger.next;
+            }
+            head = head.next;
+        }
+        bigger.next = null;
+        smaller.next = dummy2.next;
+        return  dummy1.next;
+    }
+}
