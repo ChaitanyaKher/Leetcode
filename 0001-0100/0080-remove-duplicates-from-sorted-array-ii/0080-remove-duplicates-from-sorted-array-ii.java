1157d7f3d3b8580d3db0d2e361e672f22e47a7c9
Chaitanya Kher <chaitanyakher2131996@gmail.com>
2026-09-22 07:33:58 +0530

    Solved 0080: Remove Duplicates From Sorted Array Ii

---
+++ 0001-0100/0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int res = 0;
+
+        for(int num: nums) {
+            if(res<2||num>nums[res-2]){
+                nums[res++]=num;
+            }
+        }
+
+        return res;
+    }
+}
