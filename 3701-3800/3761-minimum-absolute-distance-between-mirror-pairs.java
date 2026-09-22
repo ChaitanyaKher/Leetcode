97bc96940566c418585952099cbeac6e370a1130
Chaitanya Kher <chaitanyakher2131996@gmail.com>
2026-09-22 07:06:52 +0530

    Solved 3761: Minimum Absolute Distance Between Mirror Pairs

---
+++ 3701-3800/3761-minimum-absolute-distance-between-mirror-pairs.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=3761 lang=java
+ *
+ * [3761] Minimum Absolute Distance Between Mirror Pairs
+ */
+
+// @lc code=start
+
+import java.util.Hashtable;
+
+class Solution {
+    public int minMirrorPairDistance(int[] nums) {
+        Hashtable<Integer, Integer> mirrorPairs = new Hashtable<>();
+        for(int i: nums) {
+
+        }
+    }
+}
+// @lc code=end
+
