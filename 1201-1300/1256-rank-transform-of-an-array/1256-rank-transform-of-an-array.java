3a515ec9fe0fc136125fa9e470df8c2259f0941f
Chaitanya Kher <chaitanyakher2131996@gmail.com>
2026-09-22 07:34:06 +0530

    Solved 1256: Rank Transform Of An Array

---
+++ 1201-1300/1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
+        Map<Integer, Integer> rank = new HashMap<>();
+        int curRank = 1;
+
+        for (int x : sorted) {
+            if (!rank.containsKey(x))
+                rank.put(x, curRank++);
+        }
+
+        int[] ans = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++)
+            ans[i] = rank.get(arr[i]);
+
+        return ans;
+    }
+}
