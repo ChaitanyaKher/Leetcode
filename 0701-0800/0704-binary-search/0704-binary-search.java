77b327842720682a6e995fbdf5d35d050a0f58ec
Chaitanya Kher <chaitanyakher2131996@gmail.com>
2026-09-22 23:06:42 +0530

    Solved 0704: Binary Search

--- 0701-0800/0704-binary-search/0704-binary-search.java
+++ 0701-0800/0704-binary-search/0704-binary-search.java
@@ -7,7 +7,21 @@
 // @lc code=start
 class Solution {
     public int search(int[] nums, int target) {
-        
+        int left = 0;
+        int right = nums.length-1;
+
+        while (left<=right) {
+            int pivot = left+ (right-left)/2;
+            if (nums[pivot] == target) {
+                return pivot;
+            } else if (nums[pivot]>target) {
+                right = pivot-1;
+            } else {
+                left = pivot+1;
+            }
+        }
+
+        return -1;
     }
 }
 // @lc code=end
