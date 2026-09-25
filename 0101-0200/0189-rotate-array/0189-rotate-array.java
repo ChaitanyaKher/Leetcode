5962dd59ab6c94aa2c1993f0d72662c4634f0639
Chaitanya Kher <chaitanyakher2131996@gmail.com>
2026-09-25 08:27:24 +0530

    Solved 0189: Rotate Array

--- 0101-0200/0189-rotate-array/0189-rotate-array.java
+++ 0101-0200/0189-rotate-array/0189-rotate-array.java
@@ -7,7 +7,23 @@
 // @lc code=start
 class Solution {
     public void rotate(int[] nums, int k) {
-        
+        if (k == 0 || nums.length == 0) {
+            return;
+        }
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+     private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 }
 // @lc code=end
